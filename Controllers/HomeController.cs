3dbc69f0863576dcec5f040d33cd99993fe2793d
sol <shaheem.nazami@gmail.com>
2013-11-12 22:01:04 +0530

    commi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using demoGrid.GridHelper;
+using demoGrid.Models;
+
+namespace demoGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult EditPopUp()
+        {
+            return PartialView();
+        }
+
+
+        [HttpGet]
+        public ActionResult GetAccountList(GridSettings gridSettings)
+        {
+            // create json data
+            int totalRecords = 1000;
+            int totalPages = (int)Math.Ceiling((float)totalRecords / (float)gridSettings.pageSize);
+            int startRow = (gridSettings.pageIndex - 1) * gridSettings.pageSize;
+            int endRow = startRow + gridSettings.pageSize;
+
+            List<Account> list = new List<Account>();
+            for (int iX = startRow; iX < endRow; iX++)
+            {
+                Account account = new Account();
+                account.AccountNumber = string.Format("Account# {0}", iX);
+                account.AccountText = "Line# " + iX.ToString();
+                account.AccountDropdown = ((Convert.ToSingle(iX) % 2 == 0) ? "USA" : "England");
+                account.AccountDate = DateTime.Now.ToShortDateString();
+                account.AccountType = ((Convert.ToSingle(iX) % 2 == 0) ? "Yes" : "No");
+                account.AccountBalance = "12345678.90";
+                list.Add(account);
+            }
+
+            var jsonData = new
+            {
+                total = totalPages,
+                page = gridSettings.pageIndex,
+                records = totalRecords,
+
+                rows =
+                (
+                    from Account item in list
+                    select new
+                    {
+                        id = item.Id,
+                        cell = new object[]
+                        {
+                            item.AccountNumber,
+                            item.AccountText,
+                            item.AccountDropdown,
+                            item.AccountDate,
+                            item.AccountType,
+                            item.AccountBalance
+                        }
+                    }
+                ).ToArray()
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+
+        public class Account
+        {
+            public int Id { get; set; }
+            public string AccountNumber { get; set; }
+            public string AccountText { get; set; }
+            public string AccountDropdown { get; set; }
+            public string AccountDate { get; set; }
+            public string AccountType { get; set; }
+            public string AccountBalance { get; set; }
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PopController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using demoGrid.Common;
+using System.Threading;
+
+namespace demoGrid.Controllers
+{
+    public class PopController : Controller
+    {
+        //
+        // GET: /Pop/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, FileDownload]
+        public FilePathResult DownloadReport(int id)
+        {
+            return GetReport(id);
+        }
+
+        private FilePathResult GetReport(int id)
+        {
+            //simulate generating the report
+            Thread.Sleep(3000);
+
+            return File("~/Report.pdf", "application/pdf", string.Format("Report{0}.pdf", id));
+        }
+
+        public ActionResult OpenLogin()
+        {
+            return PartialView("LoginDialog");
+        }
+
+       
+        // perform login
+        public ActionResult Login(string username, string password)
+        {
+            return Json(new { Success = true });
+        }
+        
+    }
+}
